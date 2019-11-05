c6b0baeb733d7cf7827c5ac69d2db9575db085f6
Soumitra Pal <soumitrakp@gmail.com>
2019-11-05 15:01:51 -0500

    update path

--- examples/example2.R
+++ examples/example2.R
@@ -1,15 +1,15 @@
 library(EvoGeneX)
 
-data = read.csv('../pkgdata/HD_M_FBgn0000008.csv', row.names=1, stringsAsFactors=F)
+data = read.csv('../data/HD_M_FBgn0000008.csv', row.names=1, stringsAsFactors=F)
 print(data)
 
 evog <- EvoGeneX()
-evog$setTree('../pkgdata/drosophila9.newick')
-evog$setRegimes('../pkgdata/regime_fruitveg.csv')
+evog$setTree('../data/drosophila9.newick')
+evog$setRegimes('../data/regime_fruitveg.csv')
 res = evog$fit(data, alpha=0.1, gamma.sq=0.01)
 print(res)
 
 brown <- Brown()
-brown$setTree('../pkgdata/drosophila9.newick')
+brown$setTree('../data/drosophila9.newick')
 res = brown$fit(data, gamma.sq=0.01)
 print(res)
