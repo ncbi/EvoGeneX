89fd471c2dcff8320e0eca149e99ce18f97e23c4
Soumitra Pal <soumitrakp@gmail.com>
2022-05-30 17:59:29 -0400

    created vignette and addred drosophila data

---
+++ Rpackage/R/DrosophilaData.R
@@ -0,0 +1,230 @@
+#' Gene expression (bulk RNA-seq) form female head of nine Drosophila species
+#'
+#' Replicated gene expression (bulk RNA-seq) data from the female head of the
+#' following 9 drosophila species (in 4 replicates: R1, R2, R3, R4):
+#'   D. melanogaster (dmel)
+#'   D. yakuba (dyak)
+#'   D. ananassae (dana)
+#'   D. pseudoobscura (dpse)
+#'   D. persimilis (dper)
+#'   D. willistoni (dwil)
+#'   D. virilis (dvir)
+#'   D. mojavensis (dmoj)
+#'   D. grimshawi (dgri)
+#'
+#' @format A data frame with 8591 rows and 38 columns. First two columns
+#' \code{FBgnDmel}, and \code{SymbolDmel} give the flybase gene ids and symbols
+#' of the 8591 orthologs in D. melanogaster. The rest of the 36 columns in the
+#' form \code{F_HD_<species>_<replicate>} give the normalized gene expression
+#' of each ortholog in 4 replicates of each of the 9 species.
+#'
+#' @source \url{https://doi.org/10.26508/lsa.201800156}
+"FemaleHead"
+
+#' Gene expression (bulk RNA-seq) form male head of nine Drosophila species
+#'
+#' Replicated gene expression (bulk RNA-seq) data from the male head of the
+#' following 9 drosophila species (in 4 replicates: R1, R2, R3, R4):
+#'   D. melanogaster (dmel)
+#'   D. yakuba (dyak)
+#'   D. ananassae (dana)
+#'   D. pseudoobscura (dpse)
+#'   D. persimilis (dper)
+#'   D. willistoni (dwil)
+#'   D. virilis (dvir)
+#'   D. mojavensis (dmoj)
+#'   D. grimshawi (dgri)
+#'
+#' @format A data frame with 8591 rows and 38 columns. First two columns
+#' \code{FBgnDmel}, and \code{SymbolDmel} give the flybase gene ids and symbols
+#' of the 8591 orthologs in D. melanogaster. The rest of the 36 columns in the
+#' form \code{M_HD_<species>_<replicate>} give the normalized gene expression
+#' of each ortholog in 4 replicates of each of the 9 species.
+#'
+#' @source \url{https://doi.org/10.26508/lsa.201800156}
+"MaleHead"
+
+#' Gene expression (bulk RNA-seq) form female thorax of nine Drosophila species
+#'
+#' Replicated gene expression (bulk RNA-seq) data from the female thorax of the
+#' following 9 drosophila species (in 4 replicates: R1, R2, R3, R4):
+#'   D. melanogaster (dmel)
+#'   D. yakuba (dyak)
+#'   D. ananassae (dana)
+#'   D. pseudoobscura (dpse)
+#'   D. persimilis (dper)
+#'   D. willistoni (dwil)
+#'   D. virilis (dvir)
+#'   D. mojavensis (dmoj)
+#'   D. grimshawi (dgri)
+#'
+#' @format A data frame with 8591 rows and 38 columns. First two columns
+#' \code{FBgnDmel}, and \code{SymbolDmel} give the flybase gene ids and symbols
+#' of the 8591 orthologs in D. melanogaster. The rest of the 36 columns in the
+#' form \code{F_TX_<species>_<replicate>} give the normalized gene expression
+#' of each ortholog in 4 replicates of each of the 9 species.
+#'
+#' @source \url{https://doi.org/10.26508/lsa.201800156}
+"FemaleThorax"
+
+#' Gene expression (bulk RNA-seq) form male thorax of nine Drosophila species
+#'
+#' Replicated gene expression (bulk RNA-seq) data from the male thorax of the
+#' following 9 drosophila species (in 4 replicates: R1, R2, R3, R4):
+#'   D. melanogaster (dmel)
+#'   D. yakuba (dyak)
+#'   D. ananassae (dana)
+#'   D. pseudoobscura (dpse)
+#'   D. persimilis (dper)
+#'   D. willistoni (dwil)
+#'   D. virilis (dvir)
+#'   D. mojavensis (dmoj)
+#'   D. grimshawi (dgri)
+#'
+#' @format A data frame with 8591 rows and 38 columns. First two columns
+#' \code{FBgnDmel}, and \code{SymbolDmel} give the flybase gene ids and symbols
+#' of the 8591 orthologs in D. melanogaster. The rest of the 36 columns in the
+#' form \code{M_TX_<species>_<replicate>} give the normalized gene expression
+#' of each ortholog in 4 replicates of each of the 9 species.
+#'
+#' @source \url{https://doi.org/10.26508/lsa.201800156}
+"MaleThorax"
+
+#' Gene expression (bulk RNA-seq) form female abdomen carcass of nine Drosophila species
+#'
+#' Replicated gene expression (bulk RNA-seq) data from the female abdomen
+#' carcass of the following 9 drosophila species (in 4 replicates: R1, R2, R3, R4):
+#'   D. melanogaster (dmel)
+#'   D. yakuba (dyak)
+#'   D. ananassae (dana)
+#'   D. pseudoobscura (dpse)
+#'   D. persimilis (dper)
+#'   D. willistoni (dwil)
+#'   D. virilis (dvir)
+#'   D. mojavensis (dmoj)
+#'   D. grimshawi (dgri)
+#'
+#' @format A data frame with 8591 rows and 38 columns. First two columns
+#' \code{FBgnDmel}, and \code{SymbolDmel} give the flybase gene ids and symbols
+#' of the 8591 orthologs in D. melanogaster. The rest of the 36 columns in the
+#' form \code{F_AC_<species>_<replicate>} give the normalized gene expression
+#' of each ortholog in 4 replicates of each of the 9 species.
+#'
+#' @source \url{https://doi.org/10.26508/lsa.201800156}
+"FemaleAbdomen"
+
+#' Gene expression (bulk RNA-seq) form male abdomen carcass of nine Drosophila species
+#'
+#' Replicated gene expression (bulk RNA-seq) data from the male abdomen carcass of the
+#' following 9 drosophila species (in 4 replicates: R1, R2, R3, R4):
+#'   D. melanogaster (dmel)
+#'   D. yakuba (dyak)
+#'   D. ananassae (dana)
+#'   D. pseudoobscura (dpse)
+#'   D. persimilis (dper)
+#'   D. willistoni (dwil)
+#'   D. virilis (dvir)
+#'   D. mojavensis (dmoj)
+#'   D. grimshawi (dgri)
+#'
+#' @format A data frame with 8591 rows and 38 columns. First two columns
+#' \code{FBgnDmel}, and \code{SymbolDmel} give the flybase gene ids and symbols
+#' of the 8591 orthologs in D. melanogaster. The rest of the 36 columns in the
+#' form \code{M_AC_<species>_<replicate>} give the normalized gene expression
+#' of each ortholog in 4 replicates of each of the 9 species.
+#'
+#' @source \url{https://doi.org/10.26508/lsa.201800156}
+"MaleAbdomen"
+
+#' Gene expression (bulk RNA-seq) form female viscera of nine Drosophila species
+#'
+#' Replicated gene expression (bulk RNA-seq) data from the female viscera of the
+#' following 9 drosophila species (in 4 replicates: R1, R2, R3, R4):
+#'   D. melanogaster (dmel)
+#'   D. yakuba (dyak)
+#'   D. ananassae (dana)
+#'   D. pseudoobscura (dpse)
+#'   D. persimilis (dper)
+#'   D. willistoni (dwil)
+#'   D. virilis (dvir)
+#'   D. mojavensis (dmoj)
+#'   D. grimshawi (dgri)
+#'
+#' @format A data frame with 8591 rows and 38 columns. First two columns
+#' \code{FBgnDmel}, and \code{SymbolDmel} give the flybase gene ids and symbols
+#' of the 8591 orthologs in D. melanogaster. The rest of the 36 columns in the
+#' form \code{F_VS_<species>_<replicate>} give the normalized gene expression
+#' of each ortholog in 4 replicates of each of the 9 species.
+#'
+#' @source \url{https://doi.org/10.26508/lsa.201800156}
+"FemaleViscera"
+
+#' Gene expression (bulk RNA-seq) form male viscera of nine Drosophila species
+#'
+#' Replicated gene expression (bulk RNA-seq) data from the male viscera of the
+#' following 9 drosophila species (in 4 replicates: R1, R2, R3, R4):
+#'   D. melanogaster (dmel)
+#'   D. yakuba (dyak)
+#'   D. ananassae (dana)
+#'   D. pseudoobscura (dpse)
+#'   D. persimilis (dper)
+#'   D. willistoni (dwil)
+#'   D. virilis (dvir)
+#'   D. mojavensis (dmoj)
+#'   D. grimshawi (dgri)
+#'
+#' @format A data frame with 8591 rows and 38 columns. First two columns
+#' \code{FBgnDmel}, and \code{SymbolDmel} give the flybase gene ids and symbols
+#' of the 8591 orthologs in D. melanogaster. The rest of the 36 columns in the
+#' form \code{M_VS_<species>_<replicate>} give the normalized gene expression
+#' of each ortholog in 4 replicates of each of the 9 species.
+#'
+#' @source \url{https://doi.org/10.26508/lsa.201800156}
+"MaleViscera"
+
+#' Gene expression (bulk RNA-seq) form ovary of nine Drosophila species
+#'
+#' Replicated gene expression (bulk RNA-seq) data from the ovary of the
+#' following 9 drosophila species (in 4 replicates: R1, R2, R3, R4):
+#'   D. melanogaster (dmel)
+#'   D. yakuba (dyak)
+#'   D. ananassae (dana)
+#'   D. pseudoobscura (dpse)
+#'   D. persimilis (dper)
+#'   D. willistoni (dwil)
+#'   D. virilis (dvir)
+#'   D. mojavensis (dmoj)
+#'   D. grimshawi (dgri)
+#'
+#' @format A data frame with 8591 rows and 38 columns. First two columns
+#' \code{FBgnDmel}, and \code{SymbolDmel} give the flybase gene ids and symbols
+#' of the 8591 orthologs in D. melanogaster. The rest of the 36 columns in the
+#' form \code{F_GO_<species>_<replicate>} give the normalized gene expression
+#' of each ortholog in 4 replicates of each of the 9 species.
+#'
+#' @source \url{https://doi.org/10.26508/lsa.201800156}
+"Ovary"
+
+#' Gene expression (bulk RNA-seq) form testis of nine Drosophila species
+#'
+#' Replicated gene expression (bulk RNA-seq) data from the testis of the
+#' following 9 drosophila species (in 4 replicates: R1, R2, R3, R4):
+#'   D. melanogaster (dmel)
+#'   D. yakuba (dyak)
+#'   D. ananassae (dana)
+#'   D. pseudoobscura (dpse)
+#'   D. persimilis (dper)
+#'   D. willistoni (dwil)
+#'   D. virilis (dvir)
+#'   D. mojavensis (dmoj)
+#'   D. grimshawi (dgri)
+#'
+#' @format A data frame with 8591 rows and 38 columns. First two columns
+#' \code{FBgnDmel}, and \code{SymbolDmel} give the flybase gene ids and symbols
+#' of the 8591 orthologs in D. melanogaster. The rest of the 36 columns in the
+#' form \code{M_GO_<species>_<replicate>} give the normalized gene expression
+#' of each ortholog in 4 replicates of each of the 9 species.
+#'
+#' @source \url{https://doi.org/10.26508/lsa.201800156}
+"Testis"
+
